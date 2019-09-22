25b0e535a5689a782720b061266278a625d4af3f
Volodymyr Horiainov <xidonett@gmail.com>
2019-09-22 19:30:50 +0200

    Начало работы с сайтом. Работа с хедером.

---
+++ main.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cheaptrips</title>
+    <link rel="stylesheet" href="css/main.css">
+    
+    <!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/70ccb402bc.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    
+    <div id="main-content">
+        <nav>
+            <ul id="nav-options" class="d-flex justify-content-between">
+                <div>
+                    <img src="resources/misc/logo.png" width="220" height="26" alt="logo">
+                    <li id="nav-first-option">Trips to Odessa</li>
+                    <li>Advantages</li>
+                    <li>Call us!</li>
+                </div>
+                <div>
+                    <p id="nav-call-number"><span></span>+38 (050) 557-65-26</p>
+                </div>
+            </ul>
+          
+        </nav>
+    </div>
+    
+    
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
